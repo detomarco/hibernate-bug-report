1c578bc39bbad75e1b02e8d68f4a7647be0082b6
Marco De Toma <marco.de.toma@fastned.nl>
2023-08-17 06:17:27 +0200

    add maven surefire

---
+++ hibernate-5-ok/src/test/kotlin/com/bugreport/service/JPAUnitCaseTest.kt
@@ -0,0 +1,74 @@
+package com.bugreport.service
+
+import javax.persistence.EntityManager
+import javax.persistence.Persistence
+import com.bugreport.service.entity.CarJava
+import com.bugreport.service.entity.CarKotlin
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class JPAUnitCaseTest {
+
+    companion object {
+        private lateinit var entityManager: EntityManager
+        private var initdb = false
+    }
+
+    @BeforeEach
+    fun beforeAll() {
+
+        if (!initdb) {
+            initdb = true
+
+            val entityManagerFactory = Persistence.createEntityManagerFactory("templatePU")
+            entityManager = entityManagerFactory!!.createEntityManager()
+            entityManager.transaction.begin()
+
+            entityManager.createNativeQuery(
+                """
+                CREATE TABLE vehicles (
+                    id uuid DEFAULT random_uuid(),
+                    vehicle_type varchar(255) not null,
+                    brand varchar(255) not null
+                );
+            """.trimIndent()
+            ).executeUpdate()
+
+            entityManager.createNativeQuery(
+                """
+               insert into vehicles (vehicle_type, brand) values ('CAR', 'Audi');
+            """.trimIndent()
+            ).executeUpdate()
+
+            entityManager.transaction.commit()
+
+        }
+
+    }
+
+    @Test
+    fun javaOK() {
+
+        val entityJava =
+            entityManager.createQuery("FROM CarJava WHERE brand = 'Audi'", CarJava::class.java)
+                .resultList.first()
+
+        assertTrue(entityJava is CarJava)
+        assertNotNull(entityJava)
+        assertNotNull(entityJava.id)
+    }
+
+    @Test
+    fun kotlinOK() {
+
+        val entityKotlin = entityManager
+            .createQuery("FROM CarKotlin WHERE brand = 'Audi'", CarKotlin::class.java)
+            .resultList.first()
+
+        assertTrue(entityKotlin is CarKotlin)
+        assertNotNull(entityKotlin)
+        assertNotNull(entityKotlin.id)
+    }
+}
